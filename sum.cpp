099dafdb0acd2c9b9b0778ea5f3fc49294c31496
wangzijian1010 <2087291150@qq.com>
2023-09-25 15:42:32 +0800

    两数之和的哈希表实现

---
+++ sum.cpp
@@ -0,0 +1,31 @@
+//
+// Created by wangzijian on 2023/9/25.
+//
+
+#include "vector"
+#include <unordered_map>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hash.count(complement)) {
+                return {hash[complement], i};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main(){
+    vector nums = {2,7,11,15};
+    Solution solution;
+    int target = 13;
+    solution.twoSum(nums,target);
+}
